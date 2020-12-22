3902c381e7a5ef3a441de8ffbf9f37ce8ac911c4
kateryna <kateryna_neholiuk@ukr.net>
2020-12-22 13:32:15 +0200

    homework 7

---
+++ php/homework7.8/pink.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> WEB mechanika #7.8 </title>
+</head>
+<p>
+Sukurkite du puslapius pink.php ir rose.php. Nuspalvinkite juos atitinkamo spalvom. 
+Į pink.php puslapį įdėkite formą su POST metodu ir mygtuku “GO TO ROSE”. Formą nukreipkite, 
+kad ji atsidarinėtų rose.php puslapyje. Padarykite taip, kad surinkus naršyklėje tiesiogiai 
+adresą į rose.php puslapį, naršyklė būtų peradresuojama į pink.php puslapį. 
+</p>
+<?php
+echo  '<style>html{background-color: pink;}</style>';
+
+
+?>
